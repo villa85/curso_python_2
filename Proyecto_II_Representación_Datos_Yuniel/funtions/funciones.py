85d9b4461afc6475736b2f53115d4c992bb357d4
villa <villalon2511@gmail.com>
2023-04-06 22:06:41 +0200

    carga de BD Canciones

--- Proyecto_II_Representación_Datos_Yuniel/funtions/funciones.py
+++ Proyecto_II_Representación_Datos_Yuniel/funtions/funciones.py
@@ -33,3 +33,19 @@ def crear_user(nombre, apellido, usuario, email):
         db.usuario.insert_many(user)
     else:
         print("El usuario ya existe...")
+
+def une_listas(l):
+
+    p = 0
+    c = len(l)
+
+    while c > 0:
+        for i in l:
+            if isinstance(i, list):
+                p = l.index(i)
+                lv = l.pop(p)
+                for j in range(len(lv)):
+                    l.insert(p, lv[j])
+                    p +=1
+        c -= 1
+    return l
--- Proyecto_II_Representación_Datos_Yuniel/main.py
+++ Proyecto_II_Representación_Datos_Yuniel/main.py
@@ -2,8 +2,30 @@ import json
 import requests
 from funtions import funciones as f
 
-f.crear_user("Yuniel", "Villalón", "villa85", "villalon2511@gmail.com")
+# f.crear_user("Yuniel", "Villalón", "villa85", "villalon2511@gmail.com")
 
+l = ['https://itunes.apple.com/search?term=System+of+a+Down&limit=5', 'https://itunes.apple.com/search?term=Marilyn+Manson&limit=5','https://itunes.apple.com/search?term=Audioslave&limit=5',
+    'https://itunes.apple.com/search?term=LINKIN+PARK&limit=5','https://itunes.apple.com/search?term=Muse&limit=5','https://itunes.apple.com/search?term=Gorillaz&limit=5',
+    'https://itunes.apple.com/search?term=Rammstein&limit=5','https://itunes.apple.com/search?term=Avenged+Sevenfold&limit=5','https://itunes.apple.com/search?term=The+Rolling+Stonesd&limit=5',
+    'https://itunes.apple.com/search?term=Limp+Bizkit&limit=5']
 
-response = requests.get('https://itunes.apple.com/search?term=System+of+a+Down')
-json_data = json.loads(response.content)
+data = {}
+data['s'] = []
+x = {}
+d= []
+
+for j in l:
+    response = requests.get(j)
+    json_data = json.loads(response.content)
+    data['s'].append(json_data["results"])
+
+m = f.une_listas(data['s'])
+
+for i in m:
+
+    x = {"nombre": i.get('trackName', i["collectionName"]), "cantante": i["artistName"], "genero": i["primaryGenreName"], "album": i["collectionName"], "url": i.get('trackViewUrl', None)}
+    d.append(x)
+
+file_name = "data\\data.json"
+with open(file_name, 'w') as file:
+    json.dump(d, file, indent=4)
